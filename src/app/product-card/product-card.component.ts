8863e637fb0b0f19ba4d64429ed5961bab90b066
oliver zulett <oliverzulett@gmail.com>
2023-04-10 10:52:20 -0400

    event binding

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -10,6 +10,29 @@ export class ProductCardComponent {
     image: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1399&q=80',
     name: 'Tasty Granite Salad',
     description: 'The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients',
-    uniAvailable: 0,
+    unitAvailable: 20,
+  }
+
+  productImageStyle = {
+    border: '1px solid black',
+    filter: 'sepia(0)'
+  }
+
+  buyProduct() {
+    console.log('compraremos un producto');
+  }
+
+  addToCart() {
+    console.log('agregaremos un producto al carro de compras');
+  }
+
+  onMouseEnter() {
+    console.log('mouse enter');
+    this.productImageStyle = {...this.productImageStyle, filter: 'sepia(0.3)'}
+  }
+
+  onMouseleave() {
+    console.log('mouse leave');
+    this.productImageStyle = {...this.productImageStyle, filter: 'sepia(0)'}
   }
 }

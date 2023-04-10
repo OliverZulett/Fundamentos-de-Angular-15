71aeb24a069c846a2c3adcad91d6a875301d9d89
oliver zulett <oliverzulett@gmail.com>
2023-04-10 08:58:20 -0400

    componentes

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,27 @@
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {
+  pageTitle = 'MY APP';
+
+  constructor() {}
+
+  onDataCheck(e: any) {
+    console.log(e);
+  }
+}
